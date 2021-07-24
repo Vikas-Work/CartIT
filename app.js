7d28de66c29c916fe75a31ce5e5cf5a002f6f60a
Vikas-Work <vikas.ml@netsmartz.net>
2021-07-24 15:48:56 +0530

    Authentication functionality

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Vicky:fallen906@cluster0.6dmki.mongodb.net/CartIT'
+
 const app = express();
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+const csrfProtection = csrf()
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,14 +30,27 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'My big secret', resave: false, saveUninitialized: false, store: store}));
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-   User.findById('60fa44c85d40f02ec8445298')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
-       next();     })
+      next();
+    })
     .catch(err => console.log(err));
- });
+});
+
+app.use( (req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,23 +60,9 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://Vicky:fallen906@cluster0.6dmki.mongodb.net/CartIT?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Vikas',
-          email: 'vikasml.2609@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
- 
-
     app.listen(3000);
   })
   .catch(err => {
